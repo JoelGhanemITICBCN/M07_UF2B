6566ce53d738e8a2e2a616e45eeea7ae3757b0ba
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2023-12-01 17:25:55 +0100

    todo de una

---
+++ my-project/app/Models/Alumne.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alumne extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'alumnat';
+    
+    protected $fillable = [
+        'name',
+        'surname',
+        'rol',
+        'email'
+    ];
+    
+    protected $hidden = [];
+}
---
+++ my-project/database/factories/ProfessorFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Centre>
+ */
+class ProfessorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name' => fake()->text(30),
+            'surname' => fake()->text(50),
+            'rol' => fake()->text(12),
+            'email' => fake()->text(55)
+        ];
+    }
+}
